7ee4a8a850a0a7bf5ce3375b8b438e660533ed1b
Mateusz <matiro28@gmail.com>
2026-05-14 16:13:43 +0200

    [#11] Hide all headers in root stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,8 +9,9 @@ Sentry.init({
 
 function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
     </Stack>
   );
 }
